c499678cbb5b8da8b4a575c71177fa0d4b50b99c
Shamar MELVIN <shamar@MacBook-Pro.local>
2023-09-06 22:41:05 -0400

    generate function + reorg

--- index.js
+++ index.js
@@ -1,127 +1,87 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const SVG = require("svg");
-const Shape = require("./lib/shapes");
-// const generateMarkdown = require("./Assets/generateMarkdown");
+const shape = require("./lib/shapes");
+const generateSvg = require("./utils/generateSvg");
 
-// var draw = SVG();
 
 //Questions array
-const questions = () => {
-    return inquirer.createPromptModule([
-        {
-            type: "input",
-            name: "textInput",
-            message: "Enter up to three characters",
-            validate: textInput => {
-                if(textInput.length <= 3) {
-                    return true;
-                } else {
-                    console.log("Please enter a maximum of 3 characters")
-                    return false;
-                }
-            }
-        },
-        {
-            type: "input", 
-            name: "txtcolor",
-            message: "Please enter a color for your text (keyword or a hexadecimal number)",
-            validate: txtColor => {
-                if(txtColor) {
-                    return true;
-                } else {
-                    console.log("Please choose a color :)")
-                    return false;
-                }
-            }
-        },
-        {
-            type: "checkbox",
-            message: "Select a shape from this list",
-            name: "shape",
-            choices: ["Circle", "Square", "Triangle"],
-            when: ({shapeConfirm}) => {
-                if(shapeConfirm) {
-                    return true;
-                } else {
-                    console.log("Pretty Please. Select a shape from the list")
-                    return false;
-                }
-            }
-        }, 
-        {
-            type: "input", 
-            name: "shapeColor",
-            message: "Select a color for your shape (keyword or hexadecimal)",
-            validate: shapeColor => {
-                if(shapeColor) {
-                    return true;
-                } else {
-                    console.log("Please choose a color for the shape")
-                    return false;
-                }
+const questions = [
+    {
+        type: "input",
+        name: "textInput",
+        message: "Enter up to three characters",
+        validate: textInput => {
+            if(textInput.length <= 3) {
+                return true;
+            } else {
+                console.log("Please enter a maximum of 3 characters")
+                return false;
             }
         }
-
-    ])
-};
-
-function generateSvg(data) {
-    return new Promise((resolve, reject) => {
-        fs.writeFile("./logo.svg", data, err => {
-            if(err) {
-                reject(err);
-                return;
+    },
+    {
+        type: "input", 
+        name: "textcolor",
+        message: "Please enter a color for your text (keyword or a hexadecimal number)",
+        validate: textColor => {
+            if(textColor) {
+                return true;
+            } else {
+                console.log("Please choose a color :)")
+                return false;
             }
+        }
+    },
+    {
+        type: "checkbox",
+        message: "Select a shape from this list:",
+        name: "shape",
+        choices: ["Circle", "Square", "Triangle"],
+        validate: (shape) => {
+            if(shape) {
+                return true;
+            } else {
+                console.log("Pretty Please. Select a shape from the list")
+                return false;
+            }
+        }
+    }, 
+    {
+        type: "input", 
+        name: "shapeColor",
+        message: "Select a color for your shape (keyword or hexadecimal)",
+        validate: shapeColor => {
+            if(shapeColor) {
+                return true;
+            } else {
+                console.log("Please choose a color for the shape")
+                return false;
+            }
+        }
+    }
 
-            resolve({
-                ok: true,
-                message: "Generated logo.svg"
-            });
-        });
-    });node 
+];
+
+
+function writeToFile(fileName, svgData) {
+    var content = generateSvg(svgData);
+    fs.writeFile(fileName, content, (err) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('Generated logo.svg successfully')
+    });
 };
 
-async function init() {
-    try {
-        const svgData = await questions();
-        const logoSvg = Shape.generateSvgString(svgData);
-        await generateSvg(logoSvg);
-        console.log("Generate logo.svg successfully!");
-    } catch (error) {
-        console.error("An error occurred:", error);
-    }
+
+function init() {
+    inquirer.prompt(questions).then((svgData) => {
+        var fileName = 'logo.svg';
+        writeToFile(fileName, svgData);
+    });
 }
 
+
 // Function call to initialize app
+
 init();
-
-
-
-// The following example test should pass:
-// ```js
-// const shape = new Triangle();
-// shape.setColor("blue");
-// expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
-
-// function Shape(shape, setColor) {
-//     this.shape = [Triangle, Circle, Square];
-//     this.setColor = [];
-// } 
-
-// function Triangle(shape, color) {
-//     Shape.call(this, shape, color);
-//     this.shape = Triangle;
-// }
-
-// function Circle(shape, color) {
-//     Shape.call(this, shape, color);
-//     this.shape = Circle;
-// }
-
-// function Square(shape, color) {
-//     Shape.call(this, shape, color);
-//     this.shape = Square;
-// }
-
-// const shape = new Triangle(Triangle, "Blue");
---
+++ utils/generateSvg.js
@@ -0,0 +1,19 @@
+//  `Triangle`, `Circle`, and `Square` classes/packages needed
+const Circle = require('../lib/circle') 
+const Square = require('../lib/square')
+const Triangle = require('../lib/triangle')
+
+//function to generate SVG logo
+function generateSvg(svgData) {
+  let shape = undefined
+  if (svgData.shape === 'Circle') {
+    shape = new Circle (svgData.shapeColor, svgData.textInput, svgData.textColor)
+  } else if (svgData.shape == 'Square') {
+    shape = new Square (svgData.shapeColor, svgData.textInput, svgData.textColor)
+  } else {
+    shape = new Triangle (svgData.shapeColor, svgData.textInput, svgData.textColor)
+  }
+  return shape.render();
+}
+
+module.exports = generateSvg;
